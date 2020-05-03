0f81e66a216f23cc29109c7b3aadb5f9dd7d1ec3
LisaBlu33 <57621149+LisaBlu33@users.noreply.github.com>
2020-05-03 20:30:40 +0200

    Add files via upload

---
+++ fase di analisi/classVisualizzaCompitiSvolti.ts
@@ -0,0 +1,12 @@
+ class visualizzaCompitiSvolti {
+    compito: string[];
+
+    constructor(compito: string[]) {
+        //inserisco nel vettore solo i compiti terminati
+         this.compito = compito;
+    }
+
+    strainer(filtro: string){
+        //visualizza solo i compiti svolti con un determinato filtro
+    }
+ }
