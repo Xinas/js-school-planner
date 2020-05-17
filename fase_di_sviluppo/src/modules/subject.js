c87b4084dc5a65fe64ba281a32d4442cb82d4666
LisaBlu33 <57621149+LisaBlu33@users.noreply.github.com>
2020-05-17 18:06:59 +0200

    Update subject.js

--- fase_di_sviluppo/src/modules/subject.js
+++ fase_di_sviluppo/src/modules/subject.js
@@ -5,7 +5,7 @@
  * defines a type of school subject.
  * 
  * @file This file defines the Subject class.
- * @author Andrea Lanzani
+ * @author Andrea Lanzani, Lisa Pezzano
  * Everyone who modifies this file MUST add his/her name
  * @version 1.0alpha
  */
@@ -20,7 +20,55 @@ export class Subject {
   /**
    * Create a subject.
    */
-  constructor() {
 
+  /**
+   * Constructor w/ parameters.
+   * 
+   * @param {string} name    - The name of the subject.
+   * @param {boolean} isLab  - If it's or not a laboratory subject.
+   */
+  constructor(name, deadline) {
+    this.name = name;
+    this.isLab = isLab;
   }
+
+  /**
+   * Constructor w/o parameters.
+   */
+  constructor() {
+    name = "";
+    isLab = false;
+  }
+ 
+  /**
+   * Get the name of the subject.
+   * @return {string} 
+   */
+  get name() {
+    return name;
+  }
+
+  /**
+   * Set the name of the subject.
+   * @param {string} name 
+   */
+  set name(name) {
+    this.name = name;
+  }
+  /**
+   * Get if it's or not a laboratory subject.
+   * @return {boolean}
+   */
+  get isLab() {
+    return isLab;
+  }
+
+  /**
+   * Set if it's or not a laboratory subject.
+   * @param {boolean} isLab
+   */
+  set isLab(isLab) {
+    this.isLab = isLab;
+  }
+
 }
