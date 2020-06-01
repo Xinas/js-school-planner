4774f3694019a4224ac58ab714b7d4c57266f0ad
Andrea Lanzani <lanzani.andre29@gmail.com>
2020-06-01 15:39:36 +0200

    Revert "Fix code style"
    This reverts commit 831713e0b35668ba2bc18603a9681b27dcfcde94.

--- fase_di_sviluppo/src/modules/calendar.js
+++ fase_di_sviluppo/src/modules/calendar.js
@@ -11,93 +11,76 @@
  * @version 1.0alpha
  */
 
-import { Homework } from './homework.js';
-
 /**
  * @todo Implement this class.
  * @todo Write the documentation.
  * 
  * Class representing a calendar.
  */
+
 export class Calendar {
 
-  /**
-   * Create a Calendar object.
-   */
   constructor() {
-    this._homework = [];
+        this._homework = null;
   }
 
-  /**
-   * 
-   * @param {Homework} homework 
-   */
-  addHomework(homework) {
-    if (!homework.duration.isBusy()) {
-      this.homework.push(homework);
-    } else {
+  addHomework(homework){
+      
+    if(!homework.duration.isBusy()){
+        this.homework.push(homework);
+    }
+    else{
       window.alert("Could not enter assignment");
     }
+        
   }
 
-  /**
-   * 
-   * @param {*} title 
-   * @param {*} subject 
-   */
-  findHomework(title, subject) {
+  findHomework(title, subject){
+
     let element;
-    if (title != null) {
-      element = this.find(title, "title");
-    } else if (subject != null) {
-      element = this.find(subject, "subject");
-    }
+    if(title!=null)
+        element = this.find(title,"title");
+    else if(subject!= null)
+        element = this.find
+        (subject,"subject");
+
+    //element will appear somehow maybe by document.getElemetById().innerHtml or .value
   }
 
-  /**
-   * 
-   * @param {*} type 
-   * @param {*} propertis 
-   */
-  find(type, propertis) {
+   find(type,propertis){
+
     let isFind;
     let i = 0;
     isFind = false;
-    while (isFind == false && i < this.homework.length) {
-      if (this.homework[i].propertis == type) {
-        isFind = true;
-      } else {
-        i++;
-      }
+    while(isFind == false && i<this.homework.length){
+
+        if(this.homework[i].propertis == type)
+            isFind = true;
+        else 
+            i++;
     }
-    if (isFind == true) {
-      return this.homework[i];
-    } else {
-      return null;
-    }
+    if(isFind == true)
+        return this.homework[i];
+    else
+        return null;
   }
 
-  /**
-   * 
-   * @param {*} title 
-   */
-  removeHomework(title) {
+  removeHomework(title){
+
     let element;
     let index;
-    element = this.find(title, 'title');
-    if (element != null) {
-      index = this.homework.indexOf(element);
-      this.homework.splice(index, 1);
+    element = this.find(title,"title");
+    if(element!=null){
+
+        index = this.homework.indexOf(element);
+        this.homework.splice(index,1);
     }
-    else {
-      window.alert('could not remove item');
-    }
+    else
+        window.alert("could not remove item");
   }
 
-  /**
-   * @todo Implement this method.
-   */
-  toJSON() {
-    // Method's implementation.
+  toJSON(){
+
+    var JSONfile = JSON.stringify(this.homework);
   }
 }
