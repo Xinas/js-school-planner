e59ada69a5217eca23ef53e1360bebb6ffb5de17
Xinas <lanzani.andrea29@gmail.com>
2020-06-01 14:18:45 +0200

    Pre-merge fixes
    This fixes some issues before merging into master

--- fase_di_sviluppo/src/modules/subject.js
+++ fase_di_sviluppo/src/modules/subject.js
@@ -5,70 +5,64 @@
  * defines a type of school subject.
  * 
  * @file This file defines the Subject class.
- * @author Andrea Lanzani, Lisa Pezzano
+ * @author Andrea Lanzani
+ * @author Lisa Pezzano
  * Everyone who modifies this file MUST add his/her name
  * @version 1.0alpha
  */
 
 /**
- * @todo Implement this class.
- * @todo Write the documentation.
- * 
  * Class representing a school subject.
  */
 export class Subject {
+  
   /**
    * Create a subject.
-   */
-
-  /**
-   * Constructor w/ parameters.
    * 
-   * @param {string} name    - The name of the subject.
-   * @param {boolean} isLab  - If it's or not a laboratory subject.
+   * @param {string} name - The name of the subject.
+   * @param {boolean} isLab - Tells whether it's or not a laboratory subject.
    */
-  constructor(name, deadline) {
-    this.name = name;
-    this.isLab = isLab;
-  }
-
-  /**
-   * Constructor w/o parameters.
-   */
-  constructor() {
-    name = "";
-    isLab = false;
+  constructor(name, isLab) {
+    this._name = name;
+    this._isLab = isLab;
   }
  
   /**
    * Get the name of the subject.
-   * @return {string} 
+   * @return {string} The subject's name.
    */
   get name() {
-    return name;
+    return this._name;
   }
 
   /**
    * Set the name of the subject.
-   * @param {string} name 
+   * @param {string} name - The subject's new name.
    */
   set name(name) {
-    this.name = name;
+    this._name = name;
   }
   /**
    * Get if it's or not a laboratory subject.
-   * @return {boolean}
+   * @return {boolean} The subject's new option
    */
   get isLab() {
-    return isLab;
+    return this._isLab;
   }
 
   /**
    * Set if it's or not a laboratory subject.
-   * @param {boolean} isLab
+   * @param {boolean} isLab - The subject's option
    */
   set isLab(isLab) {
-    this.isLab = isLab;
+    this._isLab = isLab;
   }
-
+  
+  /**
+  * Get a string representation of the object.
+  * @return {string} The string representation of the object.
+  */
+  toString() {
+    return 'Name: ' + this._name + '\nIs laboratory: ' + this._isLab;
+  }
 }
